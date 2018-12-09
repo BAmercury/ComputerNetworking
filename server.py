55a407d2a0a1e43d5bb42b95380375b2eca4ccc8
BrianAmin <tuf49787@temple.edu>
2018-12-08 19:16:22 -0500

    Going to try the SocketServer class instead. Implementation is having trouble keeping up with client requests/respones keep getting pipeline errors

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 from socket import *
 import threading
+from SocketServer import ThreadingMixIn
  
 BUFFER_SIZE = 1024
 TCP_IP = '192.168.1.126'
@@ -10,34 +11,48 @@ TCP_PORT = 5005
 
 
 server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) 
 server_socket.bind((TCP_IP, TCP_PORT))
 server_socket.listen(1)
 
+threads = []
+
 keep_alive = True
 
-def client_callback(conn, addr):
-    keep_alive = True
-    while keep_alive:
-        data = conn.recv(BUFFER_SIZE)
-        print("received some data: ", data)
-        if (data == "Exit"):
-            keep_alive = False
-            break
-        else:
-            conn.send("Got it")
-    reply = "Bye"
-    conn.send(reply)
-    conn.close()
+class ClientCallBack(threading.Thread):
+
+    def __init__(self,ip,port):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+    def run(self):
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+
+            if (data == "Exit"):
+                break
+            else:
+                conn.send("Got It")
+        conn.send("Bye")
+        conn.close()
 
 if __name__ == '__main__':
     try:
         while keep_alive:
+            server_socket.listen(4)
             print("Waiting for clients to connect...")
-            conn, address = server_socket.accept()
+            (conn, (ip,address)) = server_socket.accept()
             print("Client connected. Address: ", address)
-            callback_thread = threading.Thread(target=client_callback(conn, address))
-            callback_thread.start()
+            newThread = ClientCallBack(ip,address)
+            newThread.start()
+            threads.append(newThread)
+            #callback_thread = threading.Thread(target=client_callback(conn, address))
+            #callback_thread.start()
+            #threads.append(callback_thread)
+        for t in threads:
+            t.join()
     except KeyboardInterrupt:
+        server_socket.close()
         pass
 
 
